e179603e3b0b8a90e772ee53c8f93c330739113f
Fabrício M. Damasceno <fabricio_wm@hotmail.com>
2019-12-30 12:41:39 -0300

    Template de Criação de Páginas (Create)

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 define('DEBUG', true);
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASSWD', 'ADMa7vek7');
+define('DB_PASSWD', 'root');
 define('DB_NAME', 'CMS_PHP_S_FW');
--- templates/admin/pages/create.tpl.php
+++ templates/admin/pages/create.tpl.php
@@ -1,5 +1,27 @@
 <h3>Administração de Páginas</h3>
 
-//Criação de páginas<br>
+<form action="" method="post">
+    <div class="form-group">
+        <label for="pagesTitle">Título</label>
+        <input name="title" id="pagesTitle" type="text" class="form-control" placeholder="Insira o Título da Página...">
+    </div>
 
-<a href="/admin">Home ADM</a>
+    <div class="form-group">
+        <label for="pagesUrl">URL</label>
+        <div class="input-group">
+        <div class="input-group-prepend">
+            <span class="input-group-text">/</span>
+        </div>        
+        <input name="url" id="pagesUrl" type="text" class="form-control" placeholder="URL Amigável, deixe em branco para informar a página inicial...">
+        </div>
+    </div>
+
+    <div class="form-group">
+        //Editor de Texto
+    </div>
+
+    <button type="submit" class="btn btn-success">Salvar</button>
+</form>
+<hr>
+
+<a href="/admin/pages" class="btn btn-info">Voltar</a>
