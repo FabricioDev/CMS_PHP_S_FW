637115155e695abcde19c3e3ba8eb5bbba781a46
Fabrício M. Damasceno <fabricio_wm@hotmail.com>
2019-12-30 13:34:18 -0300

    Template de Edição de Páginas (edit)

--- templates/admin/pages/view.tpl.php
+++ templates/admin/pages/view.tpl.php
@@ -1,5 +1,29 @@
-<h3>Administração de Páginas</h3>
+<h3 class="mb-5" >Administração de Páginas</h3>
 
-//Visualização de páginas<br>
+<div class="row">
+    <div class="col-3">
+        <dl-row>
+            <dt class="col-sm-3">Título</dt>
+            <dd class="col-sm-9">Página Inicial</dd>
 
-<a href="/admin">Home ADM</a>
+            <dt class="col-sm-3">URL</dt>
+            <dd class="col-sm-9">/ - <a href="/" target="_blank">Abrir</a></dd>
+
+            <dt class="col-sm-3">Criado em</dt>
+            <dd class="col-sm-9">30/12/2019 - 13:23</dd>
+
+            <dt class="col-sm-3">Atualizado em</dt>
+            <dd class="col-sm-9">30/12/2019 - 13:30</dd>
+        </dl-row>
+    </div>
+    <div class="col bg-light">
+        <h3>Página Inicial</h3>
+        <p>Está é a página inicial.</p>
+    </div>
+</div>
+<p>
+    <a href="/admin/pages/1/edit" class="btn btn-primary">Editar</a>
+    <a href="/admin/pages/1/edit" class="btn btn-danger">Remover</a>
+</p>
+
+<a href="/admin/pages" class="btn btn-info">Voltar</a>
