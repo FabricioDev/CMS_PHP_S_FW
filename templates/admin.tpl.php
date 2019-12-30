09b8a8199ec03e68ba841bd80dd606f3732b3c62
Fabrício M. Damasceno <fabricio_wm@hotmail.com>
2019-12-30 10:15:22 -0300

    Fazendo melhorias via css

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -7,16 +7,43 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
 
     <title>Painel Administrativo</title>
   </head>
-  <body>
-    <?php include $content ?> 
+  <body class="d-flex flex-column">
+    <div id="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="" class="navbar-brand">Admin</a>
+            <span class="navbar-text">
+                Painel Administrativo
+            </span>
+        </nav>
+    </div>
+    <div id="main">
+        <div class="row">
+            <div class="col">
+                <ul id="main-menu" class="nav flex-column nav-pills bg-secondary text-white p-2">
+                    <li class="nav-item">
+                    <a href="" class="nav-link text-white-50"><small>MENU</small></a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="" class="nav-link active">Páginas</a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="" class="nav-link">Usuário</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="content" class="col-10"><?php include $content ?></div>
+        </div>
+        
+    </div> 
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.rsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
