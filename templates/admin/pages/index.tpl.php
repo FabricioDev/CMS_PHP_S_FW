3573a451f24d826e4b9c590c91a32f295f55eff0
Fabrício M. Damasceno <fabricio_wm@hotmail.com>
2019-12-30 11:39:14 -0300

    Template de listagem de Páginas (index)

--- templates/admin/pages/index.tpl.php
+++ templates/admin/pages/index.tpl.php
@@ -1,5 +1,22 @@
-<h3>Administração de Páginas</h3>
+<h3 class="mb-5">Administração de Páginas</h3>
 
-//Listagem <br>
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Conteúdo</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td><a href="/admin/pages/1">Página Inicial</a></td>
+            <td class="text-right">
+                <a href="/admin/pages/1" class="btn btn-primary btn-sm">Ver</a>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
-<a href="/admin">Home ADM</a>
+<a href="/admin/pages/create" class="btn btn-info">Nova Página</a>
