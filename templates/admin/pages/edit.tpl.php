121ba30775ca269a756a4e308d37c06a70884ec5
Fabrício M. Damasceno <fabricio_wm@hotmail.com>
2019-12-30 10:53:31 -0300

    Rotas de Administração de páginas (CRUD)

---
+++ templates/admin/pages/edit.tpl.php
@@ -0,0 +1,5 @@
+<h3>Administração de Páginas</h3>
+
+//Edição de páginas<br>
+
+<a href="/admin">Home ADM</a>
---
+++ templates/admin/pages/index.tpl.php
@@ -0,0 +1,5 @@
+<h3>Administração de Páginas</h3>
+
+//Listagem <br>
+
+<a href="/admin">Home ADM</a>
